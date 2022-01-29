954d0f26ae012e7993ebce75ca1169b6e618c50b
Thomas Imensek <thomas.imensek@bergfex.at>
2022-01-29 16:36:37 +0100

    add register validation + add loading indicator for register;

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -14,7 +14,7 @@
     </p>
   </div>
   <form class="mt-8 space-y-6" @submit="login">
-    <div v-if="errorMsg" class="flex items-center justify-between py-3 px-5 bg-red-500 text-white rounded shadow">
+    <Alert v-if="errorMsg">
       {{errorMsg}}
       <span @click="errorMsg = ''"
             class="w-8 h-8 flex items-center justify-center rounded-full transition-colors cursor-pointer hover:bg-[rgba(0,0,0,0.2)]"
@@ -23,7 +23,7 @@
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
         </svg>
       </span>
-    </div>
+    </Alert>
 
     <input type="hidden" name="remember" value="true">
     <div class="rounded-md shadow-sm -space-y-px">
@@ -67,9 +67,9 @@
             <svg
               v-if="loading"
               class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-      </svg>
+              <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+              <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
 
           </span>
         Sign in
@@ -82,6 +82,7 @@
 import {ref} from "vue";
 import store from '../store';
 import {useRouter} from "vue-router";
+import Alert from '../components/Alert.vue';
 
 const router = useRouter();
 
--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -1,21 +1,28 @@
 <template>
   <div>
-        <img className="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-             alt="Workflow">
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Register for free
-        </h2>
-        <p class="mt-2 text-center text-sm text-gray-600">
-          Or
-          {{ ' ' }}
-          <router-link :to="{name: 'Login'}" class="font-medium text-indigo-600 hover:text-indigo-500">
-            login to your account..
-          </router-link>
-        </p>
-      </div>
+    <img className="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+         alt="Workflow">
+    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+      Register for free
+    </h2>
+    <p class="mt-2 text-center text-sm text-gray-600">
+      Or
+      {{ ' ' }}
+      <router-link :to="{name: 'Login'}" class="font-medium text-indigo-600 hover:text-indigo-500">
+        login to your account..
+      </router-link>
+    </p>
+  </div>
   <form className="mt-8 space-y-6" @submit="register">
-        <input type="hidden" name="remember" value="true">
-        <div className="rounded-md shadow-sm -space-y-px">
+    <Alert v-if="Object.keys(errors).length" class="flex-col items-stretch test-sm">
+
+      <div v-for="(field, i) of Object.keys(errors)" :key="i">
+        <div v-for="(error, ind) of errors[field] || []" :key="ind">
+          * {{error}}
+        </div>
+      </div>
+    </Alert>
+    <div className="rounded-md shadow-sm -space-y-px">
 
           <div>
             <label for="fullname" class="sr-only">FullName</label>
@@ -42,28 +49,44 @@
           </div>
         </div>
 
-        <div>
-          <button type="submit"
-                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+    <div>
+          <button
+            :disabled="loading"
+            type="submit"
+            class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            :class="{
+              'cursor-not-allowed': loading,
+              'hover:bg-indigo-500': loading,
+            }"
+          >
           <span className="absolute left-0 inset-y-0 flex items-center pl-3">
             <!-- Heroicon name: solid/lock-closed -->
-            <svg className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg"
+             <svg v-if="!loading" className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg"
                  viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path fill-rule="evenodd"
                     d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
                     clip-rule="evenodd"/>
             </svg>
+
+            <svg
+                v-if="loading"
+                class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+              <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+              <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
           </span>
             Sign up
           </button>
         </div>
-      </form>
+  </form>
 </template>
 
 <script setup>
 
 import store from '../store';
 import {useRouter} from "vue-router";
+import {ref} from "vue";
+import Alert from '../components/Alert.vue';
 
 const router = useRouter();
 
@@ -74,13 +97,24 @@ const user = {
   password_confirmation: 'tt1234',
 }
 
+const loading = ref(false);
+const errors = ref({});
+
 function register(ev) {
   ev.preventDefault();
+  loading.value = true;
   store
     .dispatch('register', user)
     .then(() => {
+      loading.value = false;
       router.push({name: 'Dashboard'})
-    });
+    })
+    .catch((err) => {
+      loading.value = false;
+      if (err.response.status === 422) {
+        errors.value = err.response.data.errors;
+      }
+  });
 
 }
 
